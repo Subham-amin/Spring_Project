1b143014cb70b5c1630a8af42ede7e37bd4bb903
SUBHAM AMIN <113018980+Subham-amin@users.noreply.github.com>
2022-12-21 19:47:06 +0530

    Delete storemanagementsystem directory

--- storemanagementsystem/src/main/java/com/store/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.store.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.store.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
